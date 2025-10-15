e107576ce2e27a387c5d22839f798426fd30d2f6
Pranav Zinzurde <pranav.z@browserstack.com>
2025-10-15 12:47:36 +0530

    fix tests

--- selenium-webdriver/tests/test.js
+++ selenium-webdriver/tests/test.js
@@ -57,7 +57,7 @@ describe("PercyOnAutomate demo test", () => {
         .findElement(By.xpath('//*[@id="1"]/p'))
         .getText();
       // clicking the 'Add to cart' button
-      await driver.findElement(By.xpath('//*[@id="3"]/div[4]')).click();
+      await driver.findElement(By.xpath('//*[@id="1"]/div[4]')).click();
       // waiting until the Cart pane has been displayed on the webpage
       await driver.wait(until.elementLocated(By.className("float-cart__content")));
       await driver.findElement(By.className("float-cart__content"));
@@ -77,7 +77,7 @@ describe("PercyOnAutomate demo test", () => {
 
 
   // checking whether product has been added to cart by comparing product name
-  expect(productCartText).toEqual(expect.stringContaining(productText));
+  expect(productText).toBe(productCartText);
     },
     10000000
   );
